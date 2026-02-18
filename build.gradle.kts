5aed12ea3947a6631e27304ecb0d95deda88e43d
rorojinx-offl <rohitgurunathan@gmail.com>
2026-02-18 18:28:42 +0000

    Added the SQLite dependency to Gradle build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("ch.qos.logback:logback-classic:1.5.28")
+    implementation("org.xerial:sqlite-jdbc:3.51.2.0")
 }
 
 tasks.test {
