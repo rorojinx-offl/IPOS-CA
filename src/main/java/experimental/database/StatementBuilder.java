9cf0cbae1cc5c20e881991c6280063fbdbb78654
rorojinx-offl <rohitgurunathan@gmail.com>
2026-02-23 14:02:24 +0000

    Experiments with SQLite

--- src/main/java/experimental/database/StatementBuilder.java
+++ src/main/java/experimental/database/StatementBuilder.java
@@ -1,19 +1,24 @@
 package experimental.database;
 
 import java.util.ArrayList;
+import java.util.StringJoiner;
 
 public class StatementBuilder {
     public String createTableStm(String tableName, ArrayList<DatabaseOps.Table> tableSchema) {
-        String stm = "";
-        stm = stm.concat(String.format("CREATE TABLE IF NOT EXISTS %s (", tableName));
+        /*String stm = "";
+        stm = stm.concat(String.format("CREATE TABLE IF NOT EXISTS %s (", tableName));*/
+        StringBuilder stm = new StringBuilder();
+        stm.append(String.format("CREATE TABLE IF NOT EXISTS %s (", tableName));
 
+        int i = 1;
         for (DatabaseOps.Table tuple : tableSchema) {
             String pk = tuple.isPrimaryKey() ? "PRIMARY KEY" : "";
             String notNull = tuple.isNotNull() ? "NOT NULL" : "";
-            String isEnd = tableSchema.indexOf(tuple) == tableSchema.size() - 1 ? ")" : ", ";
+            String isEnd = i == tableSchema.size() ? ")" : ", ";
 
-            stm = stm.concat(String.format("%s %s %s %s%s", tuple.tupleName(), tuple.type(), pk, notNull, isEnd));
+            stm.append(String.format("%s %s %s %s%s", tuple.tupleName(), tuple.type(), pk, notNull, isEnd));
+            i++;
         }
-        return stm;
+        return stm.toString();
     }
 }
