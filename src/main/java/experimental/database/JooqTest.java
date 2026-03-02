fdefdcb519f7b69206c147f838a93a859a582f0b
rohitg-city <rohitgurunathan@gmail.com>
2026-03-02 18:23:08 +0000

    jooq select query

--- src/main/java/experimental/database/JooqTest.java
+++ src/main/java/experimental/database/JooqTest.java
@@ -6,7 +6,7 @@ import static experimental.database.generated.tables.Company.COMPANY;
 public class JooqTest {
     static void main() {
         DSLContext ctx = JooqConnection.getDSLContext();
-        insertQuery(ctx);
+        selectQuery(ctx);
     }
 
     private static void insertQuery(DSLContext ctx) {
@@ -15,6 +15,13 @@ public class JooqTest {
                 .set(COMPANY.AGE, 29)
                 .set(COMPANY.ADDRESS, "London")
                 .set(COMPANY.SALARY, 90000f)
+                .execute();
+    }
+
+    private static void selectQuery(DSLContext ctx) {
+        ctx.select(COMPANY.AGE)
+                .from(COMPANY)
+                .where(COMPANY.NAME.eq("Rohit"))
                 .execute();
     }
 }
