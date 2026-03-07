934781370581e0e99c2d5068613882072e7b2aa3
rohitg-city <rohitgurunathan@gmail.com>
2026-03-07 15:20:18 +0000

    foo

---
+++ src/main/java/org/novastack/iposca/utils/common/Test.java
@@ -0,0 +1,36 @@
+package org.novastack.iposca.utils.common;
+
+import org.novastack.iposca.utils.db.ColumnConstraint;
+import org.novastack.iposca.utils.db.SQLiteConnection;
+import org.novastack.iposca.utils.db.DDLEngine;
+import org.novastack.iposca.utils.db.TableSchema;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Test {
+    static void main() {
+        Connection conn = new SQLiteConnection().getConnection();
+
+        DDLEngine ddl = new DDLEngine();
+        try {
+            ddl.createTable(conn,"flexi_dsc",initiateTables());
+            ddl.createIndex(conn,"flexi_dsc","CUST_ID");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static ArrayList<TableSchema> initiateTables() {
+        return new ArrayList<>() {
+            {
+                add(new TableSchema.Column("CUST_ID", "INTEGER", true, true,null));
+                add(new TableSchema.Column("RATE", "INTEGER", false, true,new ArrayList<>() {{
+                    add(new ColumnConstraint.Default("0"));
+                }}));
+                add(new TableSchema.ForeignKey("CUST_ID", "customer", "ID", true, true));
+            }
+        };
+    }
+}
