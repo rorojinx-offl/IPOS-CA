92131931fd740048452f945833b016b64a3bdb23
rorojinx-offl <rohitgurunathan@gmail.com>
2026-03-09 13:11:01 +0000

    ffurr0foe

---
+++ src/main/java/org/novastack/iposca/utils/ui/IValid.java
@@ -0,0 +1,31 @@
+package org.novastack.iposca.utils.ui;
+
+public class IValid {
+    public static boolean checkCreditLimit(String creditLimit) {
+        float limit;
+        try {
+            limit = Float.parseFloat(creditLimit);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return limit > 0;
+    }
+
+    public static boolean checkEmail(String email) {
+        return email.matches("^[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}$");
+    }
+
+    public static boolean checkPhone(String phone) {
+        return phone.matches("^\\d{11}$");
+    }
+
+    public static boolean checkRate(String rate) {
+        int rateInt;
+        try {
+            rateInt = Integer.parseInt(rate);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return rateInt >= 0 && rateInt <= 100;
+    }
+}
