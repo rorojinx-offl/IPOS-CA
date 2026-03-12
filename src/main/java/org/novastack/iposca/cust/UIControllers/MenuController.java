95f66defd603b3460fb39ef26cd6778a9be8bdf6
rorojinx-offl <rohitgurunathan@gmail.com>
2026-03-12 17:29:50 +0000

    Highlight areas that have been hovered over

--- src/main/java/org/novastack/iposca/cust/UIControllers/MenuController.java
+++ src/main/java/org/novastack/iposca/cust/UIControllers/MenuController.java
@@ -2,13 +2,17 @@ package org.novastack.iposca.cust.UIControllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.novastack.iposca.utils.ui.CommonCalls;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MenuController implements Initializable {
@@ -20,6 +24,13 @@ public class MenuController implements Initializable {
     private VBox regButton;
 
     @FXML
+    private VBox debtButton;
+
+    @FXML
+    private VBox manageButton;
+
+
+    @FXML
     void registerCustomer(MouseEvent event) throws IOException {
         Stage stage = (Stage) regButton.getScene().getWindow();
         new CommonCalls().traverse(stage, "/ui/cust/custReg.fxml");
@@ -29,5 +40,19 @@ public class MenuController implements Initializable {
     void manageCustomer(MouseEvent event) throws IOException {
         Stage stage = (Stage) regButton.getScene().getWindow();
         new CommonCalls().traverse(stage, "/ui/cust/custMgmt.fxml");
+    }
+
+    @FXML
+    void highlight(MouseEvent event) {
+        VBox option = (VBox) event.getSource();
+        Label childLabel = (Label) option.getChildren().get(1);
+        childLabel.setTextFill(Color.RED);
+    }
+
+    @FXML
+    void restoreCol(MouseEvent event) {
+        VBox option = (VBox) event.getSource();
+        Label childLabel = (Label) option.getChildren().get(1);
+        childLabel.setTextFill(Color.BLACK);
     }
 }
