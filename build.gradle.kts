9e88c14b6428e7761fe1db177c0e7a83e738f8b0
rorojinx-offl <rohitgurunathan@gmail.com>
2026-03-12 15:50:03 +0000

    Update Build Fikeo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java")
     id("org.jooq.jooq-codegen-gradle") version "3.20.11"
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -17,6 +19,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.28")
     implementation("org.xerial:sqlite-jdbc:3.51.2.0")
     implementation("org.jooq:jooq:3.20.11")
+    implementation("org.openjfx:javafx:25.0.2")
     jooqCodegen("org.xerial:sqlite-jdbc:3.51.2.0") //Add SQLite to task's classpath
 }
 
@@ -30,7 +33,7 @@ jooq {
     configuration {
         jdbc {
             driver = "org.sqlite.JDBC"
-            url = "jdbc:sqlite:${projectDir}/test.db"
+            url = "jdbc:sqlite:${projectDir}/iposca.db"
             user = ""
             password = ""
         }
@@ -46,9 +49,18 @@ jooq {
             }
 
             target {
-                packageName = "experimental.database.generated"
-                directory = "src/main/java/experimental/database/generated"
+                packageName = "schema"
+                directory = "src/main/java/org/novastack/iposca/utils/db/generated"
             }
         }
     }
+}
+
+javafx {
+    version = "25.0.2"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+application {
+    mainClass.set("org.novastack.iposca.cust.UIMain")
 }
