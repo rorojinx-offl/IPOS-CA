d72698aab9b39386ed5d67e67620974cadb96155
rorojinx-offl <rohitgurunathan@gmail.com>
2026-03-13 18:18:07 +0000

    Implenting restoration from in default to normal

--- src/main/java/org/novastack/iposca/cust/UIMain.java
+++ src/main/java/org/novastack/iposca/cust/UIMain.java
@@ -10,7 +10,7 @@ public class UIMain extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/ui/cust/debtRepay.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/cust/debtMgmt.fxml"));
         stage.setTitle("Customer");
         stage.setScene(new Scene(root));
         stage.show();
