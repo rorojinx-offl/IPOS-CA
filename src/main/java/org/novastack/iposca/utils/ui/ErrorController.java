e6001c93cd1076a453d3481fd8104fb1492d3a04
rorojinx-offl <rohitgurunathan@gmail.com>
2026-03-16 22:54:09 +0000

    Add files via upload

--- src/main/java/org/novastack/iposca/utils/ui/ErrorController.java
+++ src/main/java/org/novastack/iposca/utils/ui/ErrorController.java
@@ -1,11 +1,12 @@
 package org.novastack.iposca.utils.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ErrorController {
     @FXML
-    private TextField errorMsg;
+    private TextArea errorMsg;
 
     public void setError(String error) {
         errorMsg.setText(error);
