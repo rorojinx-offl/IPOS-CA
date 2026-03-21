37c22d11f126ada42ffba0f77976bd05b043ac85
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-03-21 18:11:42 +0000

    success screen

---
+++ src/main/java/org/novastack/iposca/sales/UIControllers/SuccessController.java
@@ -0,0 +1,12 @@
+package org.novastack.iposca.sales.UIControllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SuccessController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
