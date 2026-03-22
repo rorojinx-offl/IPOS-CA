5a6bfa6220a16e0dd9f7762301d9d34fc37e60b7
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-03-22 20:17:41 +0000

    Working on success screen

--- src/main/java/org/novastack/iposca/sales/UIControllers/SuccessController.java
+++ src/main/java/org/novastack/iposca/sales/UIControllers/SuccessController.java
@@ -1,12 +1,31 @@
 package org.novastack.iposca.sales.UIControllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.novastack.iposca.utils.ui.CommonCalls;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SuccessController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    void printInvoice(MouseEvent event) {
+
+    }
+
+    @FXML
+    void returnToMenu(MouseEvent event) throws IOException {
+        new CommonCalls().traverse((Stage) okButton.getScene().getWindow(), "/ui/sales/salesMenu.fxml", "Sales");
     }
 }
