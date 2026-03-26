d6e7a3ab43e7adc19f173cad527036225d1e8ee1
Hetal <hetal.patel.3@city.ac.uk>
2026-03-26 22:53:15 +0000

    In testJooq, I created fucntions createItem, updateQauntity, getAllItems, deleteItems and practised with some test data but there is on issue with the updateQuanityt fucntion that needs to be checked . I alsoe made a List to hold stock items

--- src/main/java/org/novastack/iposca/stock/Stock.java
+++ src/main/java/org/novastack/iposca/stock/Stock.java
@@ -1,15 +1,47 @@
 package org.novastack.iposca.stock;
-
+//creating stock objects
 public class Stock {
     private int id;
     private String name;
-    private float price;
+    private float bulkCost;
     private int quantity;
 
-    public Stock(String name) {
+    public Stock(int id, String name, float bulkCost, int quantity) {
         this.id = id;
         this.name = name;
-        this.price = price;
+        this.bulkCost = bulkCost;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getbulkCost() {
+        return bulkCost;
+    }
+
+    public void setBulkCost(float price) {
+        this.bulkCost = bulkCost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 }
--- src/main/java/org/novastack/iposca/stock/TestJooq.java
+++ src/main/java/org/novastack/iposca/stock/TestJooq.java
@@ -1,36 +1,63 @@
 package org.novastack.iposca.stock;
 
 import org.jooq.DSLContext;
+import org.jooq.Result;
 import org.novastack.iposca.utils.db.JooqConnection;
+import schema.tables.records.StockTableRecord;
 
-import static java.lang.IO.print;
+import java.util.List;
+
 import static schema.tables.StockTable.STOCK_TABLE;
 
 public class TestJooq {
-    public static void main() {
+    public static void main(String[] args) {
         DSLContext ctx = JooqConnection.getDSLContext();
-        ctx.insertInto(STOCK_TABLE)
-                .set(STOCK_TABLE.NAME, "Paracetamol")
-                .set(STOCK_TABLE.BULK_COST, 50f)
-                .set(STOCK_TABLE.QUANTITY, 300)
+
+        createItem(ctx, "Paracetomal", 50f, 300);
+        createItem(ctx, "Aspirin", 40f, 500);
+
+        List<Stock> items = getAllItems(ctx);
+        for (Stock item : items) {
+            System.out.println(
+                    item.getId() + " | " +
+                            item.getName() + " | " +
+                            item.getbulkCost() + " | " +
+                            item.getQuantity()
+            );
+        }
+
+        updateQuantity(ctx,"Paracetamol", 350);
+        deleteItem(ctx, "Aspirin");
+    }
+    public static int createItem(DSLContext ctx, String name, float bulkCost, int quantity) {
+        return ctx.insertInto(STOCK_TABLE)
+                .set(STOCK_TABLE.NAME, name)
+                .set(STOCK_TABLE.BULK_COST, bulkCost)
+                .set(STOCK_TABLE.QUANTITY, quantity)
                 .execute();
-        ctx.insertInto(STOCK_TABLE)
-                .set(STOCK_TABLE.NAME, "Aspirin")
-                .set(STOCK_TABLE.BULK_COST, 50f)
-                .set(STOCK_TABLE.QUANTITY, 500)
+    }
+
+    public static List<Stock> getAllItems(DSLContext ctx) {
+        Result<StockTableRecord> rows = ctx.selectFrom(STOCK_TABLE).fetch();
+
+        return rows.map(row -> new Stock(
+                row.getItemId(),
+                row.getName(),
+                row.getBulkCost(),
+                row.getQuantity()
+        ));
+    }
+
+    public static int updateQuantity(DSLContext ctx, int itemId, int newQuantity) {
+        return ctx.update(STOCK_TABLE)
+                .set(STOCK_TABLE.QUANTITY, newQuantity)
+                .where(STOCK_TABLE.ITEM_ID.eq(itemId))
                 .execute();
-        ctx.insertInto(STOCK_TABLE)
-                .set(STOCK_TABLE.NAME, "Analgin")
-                .set(STOCK_TABLE.BULK_COST, 50f)
-                .set(STOCK_TABLE.QUANTITY, 200)
-                .execute();
-        ctx.deleteFrom(STOCK_TABLE)
-                .where(STOCK_TABLE.NAME.eq("Paracetamol"))
-                .execute();
-        ctx.update(STOCK_TABLE)
-                .set(STOCK_TABLE.NAME, "Paracetamol123")
-                .set(STOCK_TABLE.BULK_COST, 50f)
-                .set(STOCK_TABLE.QUANTITY, 300)
+    }
+
+    public static int deleteItem(DSLContext ctx, int itemId) {
+        return ctx.deleteFrom(STOCK_TABLE)
+                .where(STOCK_TABLE.ITEM_ID.eq(itemId))
                 .execute();
     }
 }
