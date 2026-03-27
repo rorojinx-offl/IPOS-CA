4cdad92773b1f2ee12adb27e611f915aad59080c
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-03-27 20:15:21 +0000

    StatementInfo stores data required to generate customer statements like customer details, balance and bought items. StatementService control the table that track monthly debts of customers and helps build data for statement.

--- src/main/java/org/novastack/iposca/cust/statement/StatementInfo.java
+++ src/main/java/org/novastack/iposca/cust/statement/StatementInfo.java
@@ -1,21 +1,39 @@
 package org.novastack.iposca.cust.statement;
 
 import org.novastack.iposca.cust.customer.Customer;
+import org.novastack.iposca.sales.InvoiceItems;
 import org.novastack.iposca.sales.SaleService;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.ArrayList;
 
 public class StatementInfo {
     private Customer customer;
-    private YearMonth prevBillingPeriod;
+    private YearMonth billingMonth;
     private float balance;
-    private ArrayList<SaleService.SaleItem> items;
+    private ArrayList<StatementItems> items;
 
-    public StatementInfo(Customer customer, YearMonth prevBillingPeriod, float balance, ArrayList<SaleService.SaleItem> items) {
+    public StatementInfo(Customer customer, YearMonth billingMonth, float balance, ArrayList<StatementItems> items) {
         this.customer = customer;
-        this.prevBillingPeriod = prevBillingPeriod;
+        this.billingMonth = billingMonth;
         this.balance = balance;
         this.items = items;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public YearMonth getBillingMonth() {
+        return billingMonth;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public ArrayList<StatementItems> getItems() {
+        return items;
     }
 }
