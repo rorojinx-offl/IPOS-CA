855a1a7dbcb2cdcc36496674f207e50b81c3f919
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-03-27 20:11:58 +0000

    make new method that maps a customer's sale to the items they bought and the time period they bought it in.

--- src/main/java/org/novastack/iposca/sales/SaleService.java
+++ src/main/java/org/novastack/iposca/sales/SaleService.java
@@ -11,16 +11,18 @@ import org.novastack.iposca.utils.db.JooqConnection;
 import static schema.tables.Sale.SALE;
 import static schema.tables.SaleItem.SALE_ITEM;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SaleService {
     public record Sale(Integer saleID, Integer customerID, String paymentMethod, String cardVendor, String cardFirst4, String cardLast4, YearMonth cardExp, LocalDateTime saleDateTime, float saleAmount) {}
     public record SaleItem(Integer saleItemID, Integer saleID, int productID, int quantity, float price, float subtotal) {}
     public record SaleDraft(Integer customerID, ArrayList<SaleItem> items, float totalAmount) {}
-    public record SaleAggregate(Sale sale, ArrayList<SaleItem> items) {}
     public record Callback(Customer customer, ObservableList<SaleLine> cartSession, CartMode mode) {}
     public enum CartMode {
         MEMBER, GUEST
@@ -49,6 +51,55 @@ public class SaleService {
                 .set(SALE_ITEM.PRICE, saleItem.price())
                 .set(SALE_ITEM.SUBTOTAL, saleItem.subtotal())
                 .execute();
+    }
+
+    public static Map<Sale, ArrayList<SaleItem>> getSaleItems(int customerID, YearMonth month) {
+        DSLContext ctx = JooqConnection.getDSLContext();
+        LocalDateTime start = month.atDay(1).atStartOfDay().withSecond(0).withNano(0);
+        LocalDateTime end = month.plusMonths(1).atDay(1).atStartOfDay().withSecond(0).withNano(0);
+        Map<Sale, ArrayList<SaleItem>> saleMap = new HashMap<>();
+
+        ArrayList<Sale> sales = new ArrayList<>();
+        ctx.selectFrom(SALE)
+                .where(SALE.CUST_ID.eq(customerID))
+                .and(SALE.SALE_DATE_TIME.ge(parseDateTime(start)))
+                .and(SALE.SALE_DATE_TIME.lt(parseDateTime(end)))
+                .orderBy(SALE.SALE_DATE_TIME.asc())
+                .fetch().forEach(record -> {
+                    sales.add(new Sale(
+                            record.getId(),
+                            record.getCustId(),
+                            record.getPaymentMethod(),
+                            record.getCardVendor() == null ? "" : record.getCardVendor(),
+                            record.getCardFirst_4() == null ? "" : record.getCardFirst_4(),
+                            record.getCardLast_4() == null ? "" : record.getCardLast_4(),
+                            record.getCardExp() == null ? null : YearMonth.parse(record.getCardExp()),
+                            LocalDateTime.parse(record.getSaleDateTime()),
+                            record.getAmount()
+                    ));
+                });
+        if (sales.isEmpty()) {
+            System.out.println("Error with retrieving sale");
+            return null;
+        }
+
+        for (Sale sale : sales) {
+            ArrayList<SaleItem> items = new ArrayList<>();
+            ctx.selectFrom(SALE_ITEM)
+                    .where(SALE_ITEM.SALE_ID.eq(sale.saleID()))
+                    .fetch().forEach(record -> {
+                        items.add(new SaleItem(
+                                record.getId(),
+                                record.getSaleId(),
+                                record.getProductId(),
+                                record.getQuantity(),
+                                record.getPrice(),
+                                record.getSubtotal()
+                        ));
+                    });
+            saleMap.put(sale, items);
+        }
+        return saleMap;
     }
 
     public static void checkFlexiRateChange(Customer customer, float transactionAmount) {
