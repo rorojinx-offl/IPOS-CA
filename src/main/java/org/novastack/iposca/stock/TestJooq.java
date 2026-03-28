06216a04209cba1ee9f393203ec99e6b495d2b4e
Hetal <hetal.patel.3@city.ac.uk>
2026-03-28 20:27:57 +0000

    In testJooq, I moved the for loop to print all items to the getAllItems function but then i replaced it with the getAllStock fucntion to match rohits version

--- src/main/java/org/novastack/iposca/stock/TestJooq.java
+++ src/main/java/org/novastack/iposca/stock/TestJooq.java
@@ -5,6 +5,7 @@ import org.jooq.Result;
 import org.novastack.iposca.utils.db.JooqConnection;
 import schema.tables.records.StockTableRecord;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static schema.tables.StockTable.STOCK_TABLE;
@@ -16,18 +17,11 @@ public class TestJooq {
         createItem(ctx, "Paracetomal", 50f, 300);
         createItem(ctx, "Aspirin", 40f, 500);
 
-        List<Stock> items = getAllItems(ctx);
-        for (Stock item : items) {
-            System.out.println(
-                    item.getId() + " | " +
-                            item.getName() + " | " +
-                            item.getbulkCost() + " | " +
-                            item.getQuantity()
-            );
-        }
+        System.out.println(getAllStock());
 
-        updateQuantity(ctx,"Paracetamol", 350);
-        deleteItem(ctx, "Aspirin");
+        updateQuantity(ctx,1, 350);
+        deleteItem(ctx, 2);
+
     }
     public static int createItem(DSLContext ctx, String name, float bulkCost, int quantity) {
         return ctx.insertInto(STOCK_TABLE)
@@ -37,15 +31,18 @@ public class TestJooq {
                 .execute();
     }
 
-    public static List<Stock> getAllItems(DSLContext ctx) {
-        Result<StockTableRecord> rows = ctx.selectFrom(STOCK_TABLE).fetch();
-
-        return rows.map(row -> new Stock(
-                row.getItemId(),
-                row.getName(),
-                row.getBulkCost(),
-                row.getQuantity()
-        ));
+    public static ArrayList<Stock> getAllStock() {
+        ArrayList<Stock> inventory = new ArrayList<>();
+        DSLContext ctx = JooqConnection.getDSLContext();
+        ctx.selectFrom(STOCK_TABLE).where(STOCK_TABLE.QUANTITY.gt(0)).fetch().forEach(record -> {
+            inventory.add(new Stock(
+                    STOCK_TABLE.ITEM_ID.getValue(record),
+                    STOCK_TABLE.NAME.getValue(record),
+                    STOCK_TABLE.BULK_COST.getValue(record),
+                    STOCK_TABLE.QUANTITY.getValue(record)
+            ));
+        });
+        return inventory;
     }
 
     public static int updateQuantity(DSLContext ctx, int itemId, int newQuantity) {
