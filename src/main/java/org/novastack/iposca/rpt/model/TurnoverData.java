fbe56fb41157c842ab548feeb33d2c599b3c76c6
Wasil <wasil.barits@city.ac.uk>
2026-03-30 06:03:02 +0100

    Fixed TurnoverData.java typo (generatedTimestamp)

--- src/main/java/org/novastack/iposca/rpt/model/TurnoverData.java
+++ src/main/java/org/novastack/iposca/rpt/model/TurnoverData.java
@@ -9,7 +9,30 @@ public class TurnoverData {
     private int totalSalesCount;
     private float totalOrdersPlacedValue;
     private String generatedBy;
-    private LocalDate generatedTimeStamp;
+    private LocalDate generatedTimestamp;
 
     public TurnoverData() {}
+
+    public LocalDate getReportPeriodStart() { return reportPeriodStart; }
+    public void setReportPeriodStart(LocalDate reportPeriodStart) { this.reportPeriodStart = reportPeriodStart; }
+
+    public LocalDate getReportPeriodEnd() { return reportPeriodEnd; }
+    public void setReportPeriodEnd(LocalDate reportPeriodEnd) { this.reportPeriodEnd = reportPeriodEnd; }
+
+    public float getTotalSalesAmount() { return totalSalesAmount; }
+    public void setTotalSalesAmount(float totalSalesAmount) { this.totalSalesAmount = totalSalesAmount; }
+
+    public int getTotalSalesCount() { return totalSalesCount; }
+    public void setTotalSalesCount(int totalSalesCount) { this.totalSalesCount = totalSalesCount; }
+
+    public float getTotalOrdersPlacedValue() { return totalOrdersPlacedValue; }
+    public void setTotalOrdersPlacedValue(float totalOrdersPlacedValue) { this.totalOrdersPlacedValue = totalOrdersPlacedValue; }
+
+    public String getGeneratedBy() { return generatedBy; }
+    public void setGeneratedBy(String generatedBy) { this.generatedBy = generatedBy; }
+
+    public LocalDate getGeneratedTimestamp() { return generatedTimestamp; }
+    public void setGeneratedTimestamp(LocalDate generatedTimestamp) { this.generatedTimestamp = generatedTimestamp; }
+
+
 }
