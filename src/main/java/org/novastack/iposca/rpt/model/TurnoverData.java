42ad9380f1bbd054726898aaba5753fe28d21902
Wasil <wasil.barits@city.ac.uk>
2026-03-30 05:07:11 +0100

    continued TurnoverData.java

--- src/main/java/org/novastack/iposca/rpt/model/TurnoverData.java
+++ src/main/java/org/novastack/iposca/rpt/model/TurnoverData.java
@@ -3,5 +3,13 @@ package org.novastack.iposca.rpt.model;
 import java.time.LocalDate;
 
 public class TurnoverData {
-    private LocalDate report
+    private LocalDate reportPeriodStart;
+    private LocalDate reportPeriodEnd;
+    private float totalSalesAmount;
+    private int totalSalesCount;
+    private float totalOrdersPlacedValue;
+    private String generatedBy;
+    private LocalDate generatedTimeStamp;
+
+    public TurnoverData() {}
 }
