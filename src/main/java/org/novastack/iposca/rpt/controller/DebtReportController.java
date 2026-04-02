c0889b8e1da6d2d8aba5febf0b1d170616477fc1
Wasil <wasil.barits@city.ac.uk>
2026-04-02 03:59:44 +0100

    Created new controller

---
+++ src/main/java/org/novastack/iposca/rpt/controller/DebtReportController.java
@@ -0,0 +1,21 @@
+package org.novastack.iposca.rpt.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.novastack.iposca.rpt.factory.ReportFactory;
+import org.novastack.iposca.rpt.model.DebtChangeData;
+import org.novastack.iposca.rpt.service.ReportService;
+import org.novastack.iposca.utils.ui.CommonCalls;
+import org.novastack.iposca.utils.ui.ControllerTemplate;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+
+public class DebtReportController extends ControllerTemplate {
+
+}
