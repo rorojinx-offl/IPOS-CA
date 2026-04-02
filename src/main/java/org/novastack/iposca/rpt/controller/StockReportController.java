f57eab797999e218a5f9cf5d7f6e0a655da4aeb2
Wasil <wasil.barits@city.ac.uk>
2026-04-02 03:20:42 +0100

    created new controller class

---
+++ src/main/java/org/novastack/iposca/rpt/controller/StockReportController.java
@@ -0,0 +1,26 @@
+package org.novastack.iposca.rpt.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.novastack.iposca.rpt.factory.ReportFactory;
+import org.novastack.iposca.rpt.model.StockItem;
+import org.novastack.iposca.rpt.service.ReportService;
+import org.novastack.iposca.utils.ui.CommonCalls;
+import org.novastack.iposca.utils.ui.ControllerTemplate;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class StockReportController extends ControllerTemplate {
+
+}
