fbe6b4fcc5a26265df0857da777276fe243eb906
Wasil <wasil.barits@city.ac.uk>
2026-04-02 02:29:57 +0100

    new controller class

---
+++ src/main/java/org/novastack/iposca/rpt/controller/ReportsMenuController.java
@@ -0,0 +1,16 @@
+package org.novastack.iposca.rpt.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.novastack.iposca.utils.ui.CommonCalls;
+import org.novastack.iposca.utils.ui.ControllerTemplate;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class ReportsMenuController extends ControllerTemplate{
+}
