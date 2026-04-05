ab2203bfba7adcf30c6541ade3ebffa4ccbe084c
Hetal <hetal.patel.3@city.ac.uk>
2026-04-05 03:49:00 +0100

    created rateCustomisation UI, Created controller class for rate controller,

---
+++ src/main/java/org/novastack/iposca/stock/UIControllers/RateController.java
@@ -0,0 +1,43 @@
+package org.novastack.iposca.stock.UIControllers;
+
+public class RateController {
+    @FXML
+    private TableColumn<?, ?> customerID;
+
+    @FXML
+    private Button editingRate;
+
+    @FXML
+    private TableColumn<?, ?> name;
+
+    @FXML
+    private TextField newMarkupRate;
+
+    @FXML
+    private TableColumn<?, ?> rate;
+
+    @FXML
+    private Spinner<?> rateSpinner;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private TableView<?> stockTable;
+
+    @FXML
+    void changeAllVAT(MouseEvent event) {
+
+    }
+
+    @FXML
+    void changeMarkupRate(MouseEvent event) {
+
+    }
+
+    @FXML
+    void returnToParent(MouseEvent event) {
+
+    }
+
+}
