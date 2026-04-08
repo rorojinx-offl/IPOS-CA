f36df171fbe6acb3054261f32b65208c822251f9
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-04-08 21:37:08 +0100

    check config existence for the templates package

--- src/main/java/org/novastack/iposca/config/AppConfig.java
+++ src/main/java/org/novastack/iposca/config/AppConfig.java
@@ -2,6 +2,8 @@ package org.novastack.iposca.config;
 
 import org.jooq.DSLContext;
 import org.novastack.iposca.utils.db.JooqConnection;
+import schema.tables.records.AppConfigRecord;
+
 import static schema.tables.AppConfig.APP_CONFIG;
 
 public class AppConfig {
@@ -28,10 +30,10 @@ public class AppConfig {
         DSLContext ctx = JooqConnection.getDSLContext();
         ctx.insertInto(APP_CONFIG)
                 .set(APP_CONFIG.KEY, appConfig.getKey().name())
-                .set(APP_CONFIG.VALUE, appConfig.getValue())
+                .set(APP_CONFIG.VALUE, appConfig.getValue() == null ? new byte[0] : appConfig.getValue())
                 .onConflict(APP_CONFIG.KEY)
                 .doUpdate()
-                .set(APP_CONFIG.VALUE, appConfig.getValue())
+                .set(APP_CONFIG.VALUE, appConfig.getValue() == null ? new byte[0] : appConfig.getValue())
                 .execute();
     }
 
@@ -42,5 +44,17 @@ public class AppConfig {
                 .fetchOptional()
                 .map(record -> record.getValue())
                 .orElse(null);
+    }
+
+    public static boolean configExists() {
+        ConfigKey[] keys = ConfigKey.values();
+        DSLContext ctx = JooqConnection.getDSLContext();
+        for (ConfigKey key : keys) {
+            AppConfigRecord record = ctx.selectFrom(APP_CONFIG).where(APP_CONFIG.KEY.eq(key.name())).fetchOne();
+            if (record == null && key != ConfigKey.MERCHANT_LOGO && key != ConfigKey.VAT) {
+                return false;
+            }
+        }
+        return true;
     }
 }
