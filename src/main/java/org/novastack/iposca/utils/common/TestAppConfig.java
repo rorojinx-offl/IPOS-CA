400ee90f9ee68bd08fd5f32b95de100fbcb7e324
rohitg-city <rohitgurunathan@gmail.com>
2026-04-08 13:02:45 +0100

    App config logic and API that stores configs for the application such as templates, VAT rates, etc. The values in the table are stored as BLOBs so the API translates data between their primitive types and byte arrays.

---
+++ src/main/java/org/novastack/iposca/utils/common/TestAppConfig.java
@@ -0,0 +1,24 @@
+package org.novastack.iposca.utils.common;
+
+import org.novastack.iposca.config.AppConfig;
+import org.novastack.iposca.config.AppConfigAPI;
+
+public class TestAppConfig {
+    static void main() {
+        byte[] vat = AppConfigAPI.encodeInt(30);
+
+        AppConfig appConfig = new AppConfig(org.novastack.iposca.config.AppConfig.ConfigKey.VAT,vat);
+        appConfig.configure(appConfig);
+
+        int vatRate = AppConfigAPI.decodeByteToInt(AppConfig.get(AppConfig.ConfigKey.VAT));
+        System.out.println(vatRate);
+
+        byte[] mName = AppConfigAPI.enocdeString("TestPharma");
+
+        appConfig = new AppConfig(org.novastack.iposca.config.AppConfig.ConfigKey.MERCHANT_NAME,mName);
+        appConfig.configure(appConfig);
+
+        String merchantName = AppConfigAPI.decodeByteToString(AppConfig.get(AppConfig.ConfigKey.MERCHANT_NAME));
+        System.out.println(merchantName);
+    }
+}
