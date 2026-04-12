4dbb2fbb095f46fb44c4417e893b9ef9199f0c54
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-04-12 17:09:32 +0100

    The moment a stage is closed, the server is stopped to ensure proper shutdown. The splash stage does not have a titlebar and cannot be resized.

--- src/main/java/org/novastack/iposca/UIMain.java
+++ src/main/java/org/novastack/iposca/UIMain.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.novastack.iposca.http.Server;
 import org.novastack.iposca.session.SessionTimeoutManager;
 
@@ -12,13 +13,16 @@ public class UIMain extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/ui/login/login.fxml"));
-        stage.setTitle("Customer");
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/splash/splashScreen.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setResizable(false);
         stage.setScene(new Scene(root));
-        SessionTimeoutManager.install(stage);
-        Bootstrap.init();
-        Server.start();
         stage.show();
+    }
+
+    @Override
+    public void stop() {
+        Server.stop();
     }
 
     static void main(String[] args) {
