f2b8ee22a1fabf2bb84a6f52542d342115e44da7
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-04-12 17:20:11 +0100

    Exception to handle bootstrap errors

---
+++ src/main/java/org/novastack/iposca/exceptions/BootstrapException.java
@@ -0,0 +1,7 @@
+package org.novastack.iposca.exceptions;
+
+public class BootstrapException extends RuntimeException {
+    public BootstrapException(String message) {
+        super(message);
+    }
+}
