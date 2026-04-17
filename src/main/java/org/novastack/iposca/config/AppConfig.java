e4a746c3a1255c51f9314e8cf316ee263a459cf4
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-04-17 14:08:55 +0100

    javadocs and comments for config package

--- src/main/java/org/novastack/iposca/config/AppConfig.java
+++ src/main/java/org/novastack/iposca/config/AppConfig.java
@@ -6,26 +6,65 @@ import schema.tables.records.AppConfigRecord;
 
 import static schema.tables.AppConfig.APP_CONFIG;
 
+/**
+ * Class that manages the storage and retrieval of application configurations.
+ * */
 public class AppConfig {
+    /**
+     * A set of configurations that can be used in IPOS-CA.
+     * */
     public enum ConfigKey{
         VAT, MERCHANT_NAME, MERCHANT_ADDRESS, MERCHANT_EMAIL, MERCHANT_LOGO
     }
+
+    /**
+     * The key stores a defined set of configurations which are encapsulated in the enum ConfigKey.
+     * */
     private ConfigKey key;
+
+    /**
+     * The value stores the configuration value as a byte array as it is stored in the database as a BLOB which can
+     * allow us to store any kind of data.
+     * */
     private byte[] value;
 
+    /**
+     * Constructor for the AppConfig class.
+     * @param key The key of the configuration.
+     * @param value The value for the configuration.
+     * */
     public AppConfig(ConfigKey key, byte[] value) {
         this.key = key;
         this.value = value;
     }
 
+    /**
+     * Getter for the key.
+     * @return The key of the configuration which returns {@link ConfigKey}
+     * */
     public ConfigKey getKey() {
         return key;
     }
 
+    /**
+     * Getter for the value.
+     * @return The value of the configuration which returns {@code byte[]}
+     * */
     public byte[] getValue() {
         return value;
     }
 
+    /**
+     * Register a configuration into the database (upsert). {@link AppConfig} is passed in as a parameter and the config
+     * key and the pertaining value are stored in the database. If a configuration key already exists, the value is
+     * updated instead. The value is stored as a BLOB, and thus the value must be of type {@code byte[]}. The table
+     * schema does not impose a NOT NULL constraint as the UI enforces that selectively via {@link org.novastack.iposca.templates.TemplatesController}
+     * and {@link org.novastack.iposca.stock.UIControllers.RateController}. This method is best used with {@link AppConfigAPI}
+     * that will encode the primitive values into a byte array. <br>
+     * Example usage: <pre> {@code AppConfig appConfig = new AppConfig(ConfigKey.VAT, AppConfigAPI.encodeInt(10));
+     * appConfig.configure(appConfig);} </pre>
+     * @param appConfig The configuration to be stored in the database.
+     * */
     public void configure(AppConfig appConfig) {
         DSLContext ctx = JooqConnection.getDSLContext();
         ctx.insertInto(APP_CONFIG)
@@ -37,6 +76,14 @@ public class AppConfig {
                 .execute();
     }
 
+    /**
+     * Gets a configuration value from the database, with the given configuration key. If the configuration key does not
+     * exist, it returns null. This method is best used with {@link AppConfigAPI} that will decode the primitive values
+     * for use in the application. <br>
+     * Example usage: <pre> {@code int vat = AppConfigAPI.decodeByteToInt(AppConfig.get(AppConfig.ConfigKey.VAT));} </pre>
+     * @param key The configuration key to retrieve the value for.
+     * @return The value of the configuration key as {@code byte[]}, or {@code null} if the key does not exist.
+     * */
     public static byte[] get(ConfigKey key) {
         DSLContext ctx = JooqConnection.getDSLContext();
         return ctx.selectFrom(APP_CONFIG)
@@ -46,11 +93,18 @@ public class AppConfig {
                 .orElse(null);
     }
 
+    /**
+     * Checks if a full set of configurations are present in the database. Even if a single configuration is missing,
+     * the method returns false. For {@link org.novastack.iposca.templates.TemplatesController}, a false return value from
+     * this function indicates a corrupted configuration. It then prompts the user to re-configure the application.
+     * @return {@code boolean} to check if the configurations are present in the database.
+     * */
     public static boolean configExists() {
         ConfigKey[] keys = ConfigKey.values();
         DSLContext ctx = JooqConnection.getDSLContext();
         for (ConfigKey key : keys) {
             AppConfigRecord record = ctx.selectFrom(APP_CONFIG).where(APP_CONFIG.KEY.eq(key.name())).fetchOne();
+            // If the record is null, it means that the configuration is missing. But an exception is made for the logo and VAT as they are optional.
             if (record == null && key != ConfigKey.MERCHANT_LOGO && key != ConfigKey.VAT) {
                 return false;
             }
--- src/main/java/org/novastack/iposca/config/AppConfigAPI.java
+++ src/main/java/org/novastack/iposca/config/AppConfigAPI.java
@@ -3,20 +3,44 @@ package org.novastack.iposca.config;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Class that translates between primitive types (mainly ints and strings) and byte arrays.
+ * */
 public class AppConfigAPI {
+    /**
+     * Encodes a string into a byte array.
+     * @param s The string to encode.
+     * @return The encoded string as a byte array.
+     * */
     public static byte[] encodeString(String s) {
         return s.getBytes(StandardCharsets.UTF_8);
     }
 
+    /**
+     * Decodes a byte array into a string.
+     * @param b The byte array to decode.
+     * @return The decoded string.
+     * */
     public static String decodeByteToString(byte[] b) {
         return new String(b, StandardCharsets.UTF_8);
     }
 
+    /**
+     * Encodes an integer into a byte array.
+     * @param i The integer to encode.
+     * @return The encoded integer as a byte array.
+     * */
     public static byte[] encodeInt(int i) {
         return ByteBuffer.allocate(Integer.BYTES).putInt(i).array();
     }
 
+    /**
+     * Decodes a byte array into an integer.
+     * @param b The byte array to decode.
+     * @return The decoded integer.
+     * @throws IllegalArgumentException If the byte array is not of length 4.*/
     public static int decodeByteToInt(byte[] b) throws IllegalArgumentException {
+        //Check if the byte array is of length 4 to check if it is an integer.
         if (b == null || b.length != Integer.BYTES) throw new IllegalArgumentException("Expected 4 bytes for int");
 
         return ByteBuffer.wrap(b).getInt();
