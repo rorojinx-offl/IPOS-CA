f5129ec2ae14e10c3b7c113145369b2ccd1389b4
rohitg-city <rohitgurunathan@gmail.com>
2026-04-19 11:13:49 +0100

    javadocs and comments for cust.reminders package

--- src/main/java/org/novastack/iposca/cust/reminders/ReminderFactory.java
+++ src/main/java/org/novastack/iposca/cust/reminders/ReminderFactory.java
@@ -14,9 +14,23 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A factory class that generates PDF reminders for customer debt using JasperReports.
+ * */
 public class ReminderFactory {
+    /** Format for displaying dates in the PDF.*/
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
+    /**
+     * Generates a PDF reminder for a customer debt. With the given parameters, it loads the JasperReport schema and
+     * compiles it, then fills it with the built parameters, exports it to a PDF file, and opens it.
+     * @param info The information about the customer debt.
+     * @param merchant The merchant information.
+     * @param type The type of reminder to generate (first or second).
+     * @param outputPath The path to the output PDF file.
+     * @param templatePath The path to the JasperReport schema.
+     * @throws JRException If there is an error in the JasperReport compilation.
+     * */
     public static void generateReminder(ReminderInfo info, ReminderInfo.Merchant merchant, CustomerEnums.ReminderType type, Path outputPath, Path templatePath) throws JRException, IOException {
         InputStream jrxml = ReminderFactory.class.getResourceAsStream(templatePath.toString());
         if (jrxml == null) {
@@ -27,13 +41,19 @@ public class ReminderFactory {
         Map<String, Object> params = buildParams(info, merchant, type);
         JasperPrint print = JasperFillManager.fillReport(reminder, params, new JREmptyDataSource(1));
 
-
-        Files.createDirectories(outputPath.getParent());
-
         JasperExportManager.exportReportToPdfFile(print, outputPath.toString());
         PDF.openPDF(outputPath.toFile());
     }
 
+    /**
+     * Builds JasperReport parameters with the data provided by {@link #generateReminder(ReminderInfo, ReminderInfo.Merchant, CustomerEnums.ReminderType, Path, Path)}
+     * and maps the parameters ({@link String}) to a generic {@link Object} to allow for any datatype (however, the
+     * JasperReports compiler will complain if the datatype does not match the parameter type in the schema).
+     * @param info The information about the customer debt.
+     * @param merchant The merchant information.
+     * @param type The type of reminder to generate (first or second).
+     * @return A {@link Map} of parameter names ({@link String}) to their respective values ({@link Object}).
+     * */
     private static Map<String, Object> buildParams(ReminderInfo info, ReminderInfo.Merchant merchant, CustomerEnums.ReminderType type) {
         Map<String, Object> params = new HashMap<>();
 
--- src/main/java/org/novastack/iposca/cust/reminders/ReminderInfo.java
+++ src/main/java/org/novastack/iposca/cust/reminders/ReminderInfo.java
@@ -7,15 +7,34 @@ import org.novastack.iposca.cust.customer.CustomerEnums;
 import java.time.LocalDate;
 import java.time.YearMonth;
 
+/**
+ * A java bean to hold the required reminder data for reminder PDF generation by JasperReports
+ * */
 public class ReminderInfo {
     private String customerName;
     private String customerAddress;
     private String customerPhone;
-    private YearMonth issueMonthYear;
-    private LocalDate newDueDate;
-    private float debt;
+    private YearMonth issueMonthYear; // The month of the billing cycle
+    private LocalDate newDueDate; // The new due date for repayments
+    private float debt; // The debt accumulated for the customer
+    /**
+     * A record used by all Jasper-facing factory or java bean classes to store merchant information.
+     * @param name The name of the merchant.
+     * @param address The address of the merchant.
+     * @param email The email of the merchant.
+     * @param logo The logo of the merchant represented as a byte array.
+     * */
     public record Merchant(String name, String address, String email, byte[] logo) {}
 
+    /**
+     * Constructor for the ReminderInfo class.
+     * @param customerName The name of the customer.
+     * @param customerAddress The address of the customer.
+     * @param customerPhone The phone number of the customer.
+     * @param issueMonthYear The month of the billing cycle.
+     * @param newDueDate The new due date for repayments.
+     * @param debt The debt accumulated for the customer.
+     * */
     public ReminderInfo(String customerName, String customerAddress, String customerPhone, YearMonth issueMonthYear, LocalDate newDueDate, float debt) {
         this.customerName = customerName;
         this.customerAddress = customerAddress;
@@ -25,6 +44,14 @@ public class ReminderInfo {
         this.debt = debt;
     }
 
+    /**
+     * Factory method to create a ReminderInfo object from a {@link CustomerDebt} object and a given reminder type.
+     * From the {@link CustomerDebt} object, we retrieve the customer's ID and from that we can retrieve a {@link Customer}
+     * object to get the customer's address and phone number. We also retrieve the customer's reminder date, based on
+     * the provided reminder type. We then get the balance of the customer's debt.
+     * @param cd The customer debt object.
+     * @param type The type of reminder.
+     * */
     public static ReminderInfo setReminderInfo(CustomerDebt cd, CustomerEnums.ReminderType type) {
         Customer c = new Customer().getCustomer(cd.getCustomerID());
         String address = c.getAddress();
