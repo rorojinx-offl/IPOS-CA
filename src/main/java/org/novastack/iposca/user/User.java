8916c447f8a2ec4a07468a87809a6007657decd6
Rohit Gurunathan <rohitgurunathan@gmail.com>
2026-04-19 21:36:17 +0100

    javadocs and comments for user package

--- src/main/java/org/novastack/iposca/user/User.java
+++ src/main/java/org/novastack/iposca/user/User.java
@@ -12,6 +12,9 @@ import java.util.List;
 
 import static schema.tables.User.USER;
 
+/**
+ * Class that represents a user in the system.
+ * */
 public class User {
     private int id;
     private String username;
@@ -20,6 +23,15 @@ public class User {
     private String fullName;
     private LocalDate createdAt;
 
+    /**
+     * Constructor for the User class that is used to authenticate and fetch all users.
+     * @param id The ID of the user.
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @param role The role of the user.
+     * @param fullName The full name of the user.
+     * @param createdAt The date and time when the user was created.
+     * */
     public User(int id, String username, String password, UserEnums.UserRole role, String fullName, LocalDate createdAt) {
         this.id = id;
         this.username = username;
@@ -29,6 +41,14 @@ public class User {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Constructor for the User class that is used to create users.
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @param role The role of the user.
+     * @param fullName The full name of the user.
+     * @param createdAt The date and time when the user was created.
+     * */
     public User(String username, String password, UserEnums.UserRole role, String fullName, LocalDate createdAt) {
         this.username = username;
         this.password = password;
@@ -37,6 +57,10 @@ public class User {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Adds a new user to the database.
+     * @param user The user to be added.
+     * */
     public void createUser(User user) {
         DSLContext ctx = JooqConnection.getDSLContext();
         ctx.insertInto(USER)
@@ -48,6 +72,10 @@ public class User {
                 .execute();
     }
 
+    /**
+     * Fetches all users from the database.
+     * @return A list of {@link User} objects representing all users.
+     * */
     public static List<User> getAllUsers() {
         DSLContext ctx = JooqConnection.getDSLContext();
         return ctx.selectFrom(USER)
@@ -62,6 +90,10 @@ public class User {
                 ));
     }
 
+    /**
+     * Deletes a user from the database based on their ID.
+     * @param id The ID of the user to be deleted.
+     * */
     public static void deleteUserById(int id) {
         DSLContext ctx = JooqConnection.getDSLContext();
         ctx.deleteFrom(USER)
@@ -69,6 +101,12 @@ public class User {
                 .execute();
     }
 
+    /**
+     * Updates the role of a user in the database.
+     * @param id The ID of the user to be updated.
+     * @param role The new role of the user.
+     * @throws AuthenticationException If the user is trying to change their own role while logged in.
+     * */
     public static void updateUserRole(int id, UserEnums.UserRole role) throws AuthenticationException {
         if (SessionManager.getCurrentSession().getCurrentUser().getId() == id) {
             throw new AuthenticationException("Cannot change your own role while logged in!");
@@ -80,6 +118,15 @@ public class User {
                 .execute();
     }
 
+    /**
+     * Authenticates the user by getting the user record from the database, then verifying the user's existence. Then,
+     * it verifies the user's password by checking the hashed password in the database against the plaintext password.
+     * If the user is authenticated, it returns a new {@link User} object so that a session can be started.
+     * @param username The username of the user to be authenticated.
+     * @param password The password of the user to be authenticated.
+     * @return A new {@link User} object if the user is authenticated.
+     * @throws AuthenticationException If the user is not authenticated.
+     * */
     public static User authenticateUser(String username, String password) throws AuthenticationException {
         DSLContext ctx = JooqConnection.getDSLContext();
         UserRecord record = ctx.selectFrom(USER)
@@ -104,10 +151,21 @@ public class User {
         );
     }
 
+    /**
+     * Hashes a plaintext password using BCrypt.
+     * @param plaintextPwd The plaintext password to be hashed.
+     * @return The hashed password as a string.
+     * */
     private String hashPassword(String plaintextPwd) {
         return BCrypt.hashpw(plaintextPwd, BCrypt.gensalt());
     }
 
+    /**
+     * Verifies a plaintext password against a hashed password using BCrypt.
+     * @param plaintextPwd The plaintext password to be verified.
+     * @param hashedPwd The hashed password to be verified against.
+     * @return True if the plaintext password matches the hashed password, false otherwise.
+     * */
     private static boolean verifyPassword(String plaintextPwd, String hashedPwd) {
         return BCrypt.checkpw(plaintextPwd, hashedPwd);
     }
--- src/main/java/org/novastack/iposca/user/UserEnums.java
+++ src/main/java/org/novastack/iposca/user/UserEnums.java
@@ -1,10 +1,16 @@
 package org.novastack.iposca.user;
 
 public class UserEnums {
+    /**
+     * The available roles for a user.
+     * */
     public enum UserRole {
         ADMIN, PHARMACIST, MANAGER
     }
 
+    /**
+     * The packages/functionality that a user has access to.
+     * */
     public enum UserAccess {
         ORD, USER, CUST, TEMPLATES, STOCK, SALES, RPT
     }
